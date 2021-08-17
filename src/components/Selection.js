fa24bbaeb49cd8fad76a5eb5bcac8e9c2265cfa0
KADYAN <35776003+unique-kadyan@users.noreply.github.com>
2021-08-18 03:54:49 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,26 @@
 
+import React, { useEffect, useState } from "react";
+import "../styles/Child.css";
+
+export default function Selection({ applyColor }) {
+  const [color, setColor] = useState({ background: "" });
+
+  function handleClick(){
+    applyColor(setColor)
+    console.log("done")
+  }
+
+  // useEffect(()=>{
+  //   console.log(color)
+  // },[color])
+
+  return (
+    <div
+      className="fix-box"
+      style={color}
+      onClick={handleClick}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
